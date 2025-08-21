c15813f4697b98baca063658d47638000760acff
Ricardo Cavalcanti <98927024+cavalcanti30@users.noreply.github.com>
2025-08-21 18:57:49 -0300

    Add files via upload

---
+++ classes-de-um-jogo.js
@@ -0,0 +1,36 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+
+    if (this.tipo === 'mago') {
+      ataque = 'magia';
+    } else if (this.tipo === 'guerreiro') {
+      ataque = 'espada';
+    } else if (this.tipo === 'monge') {
+      ataque = 'artes marciais';
+    } else if (this.tipo === 'ninja') {
+      ataque = 'shuriken';
+    } else {
+      ataque = 'usou um ataque desconhecido';
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+
+  }
+}
+
+const heroi1 = new Heroi('Merlin', 100, 'mago');
+const heroi2 = new Heroi('Conan', 35, 'guerreiro');
+const heroi3 = new Heroi('Lee', 40, 'monge');
+const heroi4 = new Heroi('Hanzo', 25, 'ninja');
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
